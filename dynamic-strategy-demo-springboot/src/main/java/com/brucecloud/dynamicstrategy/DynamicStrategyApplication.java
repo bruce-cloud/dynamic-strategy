a9f175be5e12f2a7d545284133375601e311dcb4
Sephiroth <brucecloud@vip.qq.com>
2019-08-08 10:47:44 +0800

    initialization

---
+++ dynamic-strategy-demo-springboot/src/main/java/com/brucecloud/dynamicstrategy/DynamicStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.brucecloud.dynamicstrategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicStrategyApplication.class, args);
+	}
+
+}
